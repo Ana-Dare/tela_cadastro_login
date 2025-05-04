3a3cbc35c650a2e970457ad27a553563852e3745
Ana Daré <analauradare467@gmail.com>
2025-05-04 20:35:25 -0300

    js login criado

--- login/js/script.js
+++ login/js/script.js
@@ -4,8 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
     loginForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
-    const emailInput = document.getElementById("username-or-email").value;
-    const passwordInput = document.getElementById("password-field").value;
+    const loginUsername = document.getElementById("username-or-email").value;
+    const loginPassword = document.getElementById("password-field").value;
 
-    
-    
+    const storedUser = JSON.parse(localStorage.getItem("userData"));
+
+    if (storedUser) {
+        if ((storedUser.username === loginUsername || storedUser.email === loginUsername) && storedUser.password === loginPassword) {
+          alert("Login bem-sucedido!");
+          window.location.href = "../success/index.html"; // redireciona para a página de sucesso
+        } else {
+          alert("Usuário ou senha incorretos.");
+          loginForm.reset();
+        }
+      } 
+    });
+});
